abdb32097109faeb45e451104266b45fa528f752
Jared Gillespie <JaredLGillespie@hotmail.com>
2018-08-02 21:09:50 -0700

    Adding store unit tests

---
+++ tests/test_stores.py
@@ -0,0 +1,143 @@
+# MIT License
+#
+# Copyright (c) 2018 Jared Gillespie
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import unittest
+from unittest.mock import Mock
+from proxyscrape.scrapers import Proxy
+from proxyscrape.stores import Store
+
+
+class TestStores(unittest.TestCase):
+    def test_add_store_returns_id(self):
+        store = Store()
+        id = store.add_store()
+        self.assertIsNotNone(id)
+
+    def test_get_proxy_returns_empty_if_no_stores(self):
+        store = Store()
+        proxy = store.get_proxy()
+        self.assertIsNone(proxy)
+
+    def test_get_proxy_returns_empty_if_empty(self):
+        store = Store()
+        store.add_store()
+        proxy = store.get_proxy()
+        self.assertIsNone(proxy)
+
+    def test_get_proxy_returns_empty_if_filtered(self):
+        store = Store()
+        id = store.add_store()
+        proxy = Proxy('host', 'source', 'us', True, 'version', 'source')
+
+        store.update_store(id, {proxy, })
+        actual = store.get_proxy(filter_opts={'country': {'uk', }})
+
+        self.assertIsNone(actual)
+
+    def test_get_proxy_returns_empty_if_blacklisted(self):
+        store = Store()
+        id = store.add_store()
+        proxy = Proxy('host', 'source', 'us', True, 'version', 'source')
+
+        store.update_store(id, {proxy, })
+        actual = store.get_proxy(blacklist={proxy, })
+
+        self.assertIsNone(actual)
+
+    def test_get_proxy_returns_empty_if_fileted_and_blacklisted(self):
+        store = Store()
+        id = store.add_store()
+        proxy = Proxy('host', 'source', 'us', True, 'version', 'source')
+
+        store.update_store(id, {proxy, })
+        actual = store.get_proxy(filter_opts={'country': {'uk', }},
+                                 blacklist={proxy, })
+
+        self.assertIsNone(actual)
+
+    def test_get_proxy_returns_proxy_if_any(self):
+        store = Store()
+        id = store.add_store()
+        expected = Proxy('host', 'source', 'us', True, 'version', 'source')
+
+        store.update_store(id, {expected, })
+        actual = store.get_proxy()
+
+        self.assertEqual(expected, actual)
+
+    def test_remove_proxy_removes_from_set(self):
+        store = Store()
+        id = store.add_store()
+        proxy = Proxy('host', 'source', 'us', True, 'version', 'source')
+
+        store.update_store(id, {proxy, })
+        actual1 = store.get_proxy()
+
+        store.remove_proxy(id, proxy)
+        actual2 = store.get_proxy()
+
+        self.assertEqual(actual1, proxy)
+        self.assertIsNone(actual2)
+
+    def test_remove_proxy_invalid_id_does_nothing(self):
+        store = Store()
+        proxy = Proxy('host', 'source', 'us', True, 'version', 'source')
+        store.remove_proxy(1, proxy)
+
+        proxy = store.get_proxy()
+        self.assertIsNone(proxy)
+
+    def test_update_store_clears_if_none(self):
+        store = Store()
+        id = store.add_store()
+        proxy = Proxy('host', 'source', 'us', True, 'version', 'source')
+
+        store.update_store(id, {proxy, })
+        actual1 = store.get_proxy()
+
+        store.update_store(id, None)
+        actual2 = store.get_proxy()
+
+        self.assertEqual(proxy, actual1)
+        self.assertIsNone(actual2)
+
+    def test_update_store_updates_proxies(self):
+        store = Store()
+        id = store.add_store()
+        expected = proxy = Proxy('host', 'source', 'us', True, 'version', 'source')
+
+        store.update_store(id, {proxy, })
+        actual = store.get_proxy()
+
+        self.assertEqual(expected, actual)
+
+    def test_update_store_invalid_id_does_nothing(self):
+        store = Store()
+        proxy = Proxy('host', 'source', 'us', True, 'version', 'source')
+        store.update_store(1, proxy)
+
+        proxy = store.get_proxy()
+        self.assertIsNone(proxy)
+
+
+if __name__ == '__main__':
+    unittest.main()
